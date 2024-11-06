34fb0effe358a7000ac1afe6e2471ceb6282e0b9
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2024-11-06 16:12:52 +0100

    Added slider functionality

--- script.js
+++ script.js
@@ -24,4 +24,64 @@ menuItems.forEach(item => {
     });
 });
 
-// SLIDER
+// CAROUSEL
+
+const slides = document.querySelectorAll(".slides img");
+const indicators = document.querySelectorAll(".navigation-indicators .indicator")
+let slideIndex = 0;
+let intervalId = null;
+
+// create slider when DOM is loaded
+document.addEventListener("DOMContentLoaded", createSlider);
+
+// if slides exist, display slides
+function createSlider() {
+    if(slides.length > 0) {
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+    }
+}
+
+// start showing slides from index 0
+function showSlide(index) {
+    slideIndex = index;
+    if(index >= slides.length){
+        slideIndex = 0;
+    } else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+    updateNavIndicator();
+}
+
+// toggle indicator class depending on slide index
+function updateNavIndicator() {
+    indicators.forEach((indicator, index) => {
+        indicator.classList.toggle("active", index === slideIndex);
+    });
+}
+
+function prevSlide() {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
+}
+
+function nextSlide() {
+    clearInterval(intervalId);
+    slideIndex++;
+    showSlide(slideIndex);
+}
+
+indicators.forEach((indicator, index) => {
+    indicator.addEventListener("click", () => {
+        clearInterval(intervalId);
+        showSlide(index);
+    });
+});
+
+
+
